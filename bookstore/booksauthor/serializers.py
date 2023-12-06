7f298c67846b36d5b79e0dc73565c66afa71f70b
jahnavi0102 <jjain.rjc@gmail.com>
2023-12-07 00:46:27 +0530

    working on create books author api

--- bookstore/booksauthor/serializers.py
+++ bookstore/booksauthor/serializers.py
@@ -5,9 +5,7 @@ from .models import Books, Authors
 class AuthorsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Authors
-        fields = ('id', 'name', 'books')
-
-    books = serializers.StringRelatedField(many=True)
+        fields = ('id', 'name')
 
 
 
@@ -19,8 +17,20 @@ class BooksSerializer(serializers.ModelSerializer):
     written_by = AuthorsSerializer(many=True)
 
     def create(self, validated_data):
+        authors_data = validated_data.pop('written_by', [])
         book = Books.objects.create(**validated_data)
+        print("ok")
+        print(authors_data)
+        for author_data in authors_data:
+            # Check if author_data is a dictionary, if not, create a dictionary with 'name' key
+            if isinstance(author_data, dict):
+                author, _ = Authors.objects.get_or_create(**author_data)
+            else:
+                author, _ = Authors.objects.get_or_create(name=author_data)
+            book.written_by.add(author) 
+        print("ok1")
         book.save()
+
         return book
 
 
